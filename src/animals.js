f6ec90856b8d01ed1263aaa9db0ea5421b2da55d
hilalb-sahin <114245624+hilalb-sahin@users.noreply.github.com>
2022-10-05 21:22:16 +0100

    Update animals.js

--- src/animals.js
+++ src/animals.js
@@ -4,11 +4,11 @@ export const animals = {
       facts: ['Dolphins have been shown to give distinct names to each other!', 'Dolphins are known to display their own culture!', 'Dolphins have two stomachs!']
     },
     axolotl: {
-      image: '/images/axolotl.png',
+      image: 'images/axolotl.png',
       facts: ['Axolotl are only be found in Mexico.', 'The word Axolotl translates to  ‘water dog’.', 'Although critically endangered, the Axolotl has very few predators.']
     },
     starfish: {
-      image: '/images/starfish.png',
+      image: 'images/starfish.png',
       facts: ['Starfish can have up to 40 arms!', 'Starfish have no brain and no blood!', 'Starfish can regenerate their own arms!']
     }
   };
